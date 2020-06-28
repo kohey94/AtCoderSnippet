68359b1b6abdde05207bcf86faaa4347f191fd06
LAPTOP-7NHIC52L\kohei <sugawara622@gmail.com>
2020-06-28 20:53:38 +0900

    プロジェクト ファイルを追加します。

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AtCoderSnippet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 入力
+            var x = Console.ReadLine();
+
+            var x_y = Console.ReadLine().Split(" ");
+
+            var xInt = int.Parse(Console.ReadLine());
+            
+        }
+    }
+}
