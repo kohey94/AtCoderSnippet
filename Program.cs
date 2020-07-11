18e0e42f92ce4f0dc873577e7b76985135b7be44
KoHey94@X1C <sugawara622@gmail.com>
2020-07-11 22:43:16 +0900

    あ

--- Program.cs
+++ Program.cs
@@ -6,13 +6,42 @@ namespace AtCoderSnippet
     {
         static void Main(string[] args)
         {
-            // 入力
-            var x = Console.ReadLine();
+            var N = int.Parse(Console.ReadLine());
+            var arr = new int[N];
+            Func3(N, arr);
 
-            var x_y = Console.ReadLine().Split(" ");
+            for (int i = 0; i < N; i++)
+            {
+                Console.WriteLine(arr[i]);
+                
+            }
+        }
 
-            var xInt = int.Parse(Console.ReadLine());
-            
+        static int Func2(int x, int y, int z)
+        {
+            var q1 = x * x;
+            var q2 = y * y;
+            var q3 = z * z;
+            var q4 = x * y;
+            var q5 = y * z;
+            var q6 = z * x;
+            return q1 + q2 + q3 + q4 + q5 + q6;
+        }
+
+        static void Func3(int N, int[] arr)
+        {
+            for (int x = 1; x <= N / 6; x++)
+            {
+                for (int y = 1; y <= N / 6; y++)
+                {
+                    for (int z = 1; z <= N / 6; z++)
+                    {
+                        if ((x+y+z) >= N) break;
+                        var num = Func2(x, y, z);
+                        if (num < N) arr[num - 1] += 1;
+                    }
+                }
+            }
         }
     }
 }
